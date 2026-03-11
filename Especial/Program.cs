2cd51cc6d33bf2a6581684663277b3e5a6495da1
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-03-11 17:22:45 +0000

    Feito o primeiro exercicio

---
+++ Especial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Especial
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(GetSpecial(5));
+        }
+
+        private static int GetSpecial(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2 * GetSpecial(n - 2) + GetSpecial(n- 1);
+            }
+        }
+    }
+}
